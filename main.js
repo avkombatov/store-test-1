946a97b1fb02e73dddc5af13274b6d2b7ed0c569
kombat <avkombatov@yandex.ru>
2021-11-13 22:19:52 +0300

    вывод товаров с помощью fetch

--- main.js
+++ main.js
@@ -1,70 +1,32 @@
+const API = 'https://raw.githubusercontent.com/avkombatov/store-test-1/master';
+
 class ProductList {
     constructor(container = '.product__box') {
         this.container = container;
         this._goods = []; // полученные данные с сервера
         this._allProducts = []; // готовые объекты товаров
 
-        this._fetchGoods();
-        this._render();
+        // this._fetchGoods();
+        this.getProducts().then((data) => {
+            this._goods = [...data];
+            this._render();
+        });
+
         console.log(this.sum());
         // console.log(this);
     }
 
-    _fetchGoods() {
-        this._goods = [{
-                id: '001',
-                img: 'img/index/product/foto1.png',
-                name: 'Mango People T-shirt',
-                price: 52
-            },
-            {
-                id: '002',
-                img: 'img/index/product/foto2.png',
-                name: 'Mango People T-shirt',
-                price: 52
-            },
-            {
-                id: '003',
-                img: 'img/index/product/foto3.png',
-                name: 'Mango People T-shirt',
-                price: 52
-            },
-            {
-                id: '004',
-                img: 'img/index/product/foto4.png',
-                name: 'Mango People T-shirt',
-                price: 52
-            },
-            {
-                id: '005',
-                img: 'img/index/product/foto5.png',
-                name: 'Mango People T-shirt',
-                price: 52
-            },
-            {
-                id: '006',
-                img: 'img/index/product/foto6.png',
-                name: 'Mango People T-shirt',
-                price: 52
-            },
-            {
-                id: '007',
-                img: 'img/index/product/foto7.png',
-                name: 'Mango People T-shirt',
-                price: 52
-            },
-            {
-                id: '008',
-                img: 'img/index/product/foto8.png',
-                name: 'Mango People T-shirt',
-                price: 52
-            },
-
-        ];
+    getProducts() {
+        return fetch(`${API}/catalog.json`)
+            .then(response => response.json())
+            .catch((error) => {
+                console.log(error);
+            });
     }
+
     sum() {
-        return this._goods.reduce((sum, { 
-            price 
+        return this._goods.reduce((sum, {
+            price
         }) => sum + price, 0); // общая сумма goods
     }
     _render() {
